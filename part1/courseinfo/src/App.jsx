160eb4b98d73fb7398ea49bce4e542c8e946a780
Jari Kainulainen <128816881+jarikain@users.noreply.github.com>
2023-10-31 15:12:57 +0200

    Re-indent to 2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,65 +1,65 @@
 const Header = (props) => {
-    return (
-        <h1>{props.course.name}</h1>
-    )
+  return (
+    <h1>{props.course.name}</h1>
+  )
 }
 
 const Content = (props) => {
-    const [part1, part2, part3] = props.parts
+  const [part1, part2, part3] = props.parts
 
-    return (
-        <div>
-            <Part part={part1}/>
-            <Part part={part2}/>
-            <Part part={part3}/>
-        </div>
-    )
+  return (
+    <div>
+      <Part part={part1}/>
+      <Part part={part2}/>
+      <Part part={part3}/>
+    </div>
+  )
 }
 
 const Part = (props) => {
-    const {name, exercises} = props.part
+  const {name, exercises} = props.part
 
-    return (
-        <p> {name} {exercises} </p>
-    )
+  return (
+    <p> {name} {exercises} </p>
+  )
 }
 
 const Total = (props) => {
-    let sum = 0
-    props.parts.forEach(part => sum += part.exercises)
+  let sum = 0
+  props.parts.forEach(part => sum += part.exercises)
 
-    return (
-        <p>Number of exercises {sum}</p>
-    )
+  return (
+    <p>Number of exercises {sum}</p>
+  )
 }
 
 
 const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7
-            },
-            {
-                name: 'State of a component',
-                exercises: 14
-            }
-        ]
-    }
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
-    return (
-        <div>
-            <Header course={course}/>
-            <Content parts={course.parts}/>
-            <Total parts={course.parts}/>
-        </div>
-    )
+  return (
+    <div>
+      <Header course={course}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    </div>
+  )
 }
 
 export default App
