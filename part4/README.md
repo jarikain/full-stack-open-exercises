c51da3296cb769d2adb8750838ea91fb1a5a3614
Jari Kainulainen <128816881+jarikain@users.noreply.github.com>
2023-11-15 21:02:21 +0200

    Add exercises 4.1 and 4.2

---
+++ part4/README.md
@@ -0,0 +1,5 @@
+# My Exercises for Part 4 - Testing Express servers, user administration
+
+## [4.1-4.2](https://fullstackopen.com/en/part4/structure_of_backend_application_introduction_to_testing#exercises-4-1-4-2)
+- [x] 4.1
+- [x] 4.2
---
+++ part4/bloglist/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const { unknownEndpoint } = require('./utils/middleware')
+
+const app = express()
+
+mongoose.set('strictQuery', false)
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => logger.info('connected to MongoDB'))
+  .catch((error) => logger.error('error connecting to MongoDB', error.message))
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(unknownEndpoint)
+
+module.exports = app
