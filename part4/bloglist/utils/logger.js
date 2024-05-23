d9769d37628151326e2cef5f81b0ec48edbf43f1
Jari Kainulainen <128816881+jarikain@users.noreply.github.com>
2024-05-23 16:20:56 +0300

    Start over in part4 directory

--- part4/bloglist/utils/logger.js
+++
@@ -1,8 +0,0 @@
-const info = (...params) => console.log(...params)
-
-const error = (...params) => {
-  if (process.env.NODE_ENV !== 'test')
-    console.log(...params)
-}
-
-module.exports = { info, error }
